5237af2ed6447667922a49d312164a89ccc4d7a7
gtp <gtp0521@gmail.com>
2016-10-28 21:40:25 +0800

    first commit

---
+++ app/src/main/java/com/glemontree/duitang/ui/WelcomeActivity.java
@@ -0,0 +1,58 @@
+package com.glemontree.duitang.ui;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.glemontree.duitang.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by Administrator on 2016/10/28.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+    @InjectView(R.id.welcome_image)
+    ImageView welomeImg;
+
+    @InjectView(R.id.count_down)
+    TextView countDown;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome);
+        ButterKnife.inject(this);
+        initView();
+    }
+
+    private void initView() {
+        Picasso.with(this).load(R.drawable.welcome_img).into(welomeImg);
+        final Typeface font = Typeface.createFromAsset(getAssets(), "splash.ttf");
+        countDown.setTypeface(font);
+
+        CountDownTimer timer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                countDown.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        timer.start();
+    }
+}
